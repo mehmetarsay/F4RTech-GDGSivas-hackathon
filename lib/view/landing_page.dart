ece1fbcdf8398a39da138cc3fc7ecbd65a0dea60
FatihDursunUzer1 <fatihdursunuzer@gmail.com>
2021-02-13 01:13:57 +0300

    -LandingPage created

--- lib/view/landing_page.dart
+++ lib/view/landing_page.dart
@@ -1,5 +1,6 @@
 import 'package:f4rtech_gdgsivas_hackathon/app/colors.dart';
 import 'package:flutter/material.dart';
+
 class LandingPage extends StatefulWidget {
   @override
   _LandingPageState createState() => _LandingPageState();
@@ -9,17 +10,59 @@ class _LandingPageState extends State<LandingPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: Center(
-          child: Text(
-            'fmdogancan95@gmail.com',style: TextStyle(
-            fontStyle: FontStyle.normal,
-            fontWeight: FontWeight.normal,
-            color: ColorTable.blueT[1]
-          ),
-          ),
+      body: SafeArea(
+        child: Column(
+          children: [
+            landingText(
+                title: 'HAYIRSEVER',
+                content:
+                    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nisl eros, pulvinar facilisis justo mollis, auctor consequat urna.',
+                backgroundColor: ColorTable.blue),
+            landingText(
+                title: 'GÖNÜLLÜ',
+                content:
+                    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nisl eros, pulvinar facilisis justo mollis, auctor consequat urna.',
+                backgroundColor: ColorTable.green)
+          ],
         ),
       ),
     );
+  }
+
+  Widget landingText(
+      {@required String title,
+      @required String content,
+      @required Color backgroundColor}) {
+    return Container(
+        padding: EdgeInsets.all(16),
+        color: backgroundColor,
+        height: MediaQuery.of(context).size.height / 2.1,
+        width: MediaQuery.of(context).size.width,
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(title,
+                    style: TextStyle(
+                        fontSize: 38,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white)),
+                IconButton(
+                    icon: Icon(
+                      Icons.arrow_forward,
+                      size: 48,
+                      color: Colors.white,
+                    ),
+                    onPressed: null),
+              ],
+            ),
+            SizedBox(height: 16),
+            Text(
+              content,
+              style: TextStyle(fontSize: 24, color: Colors.white),
+            )
+          ],
+        ));
   }
 }
