c1338ce461dc1ad106f4d7b03928711d8b4eab03
Bilal Özcan <bilalozcan015@gmail.com>
2021-02-13 17:15:45 +0300

    -Product Model back-end completed

--- lib/models/product.dart
+++ lib/models/product.dart
@@ -1,37 +1,51 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:f4rtech_gdgsivas_hackathon/app/enums.dart';
 
 class Product {
   DocumentReference reference;
+  final String _id;
   final String _name;
-  final ProductType _productType;
+  final String _productType;
   final String _explanation;
   final String _publisher;
+  final GeoPoint _location;
 
-  Product(this._name, this._productType, this._explanation, this._publisher);
+  Product(this._id, this._name, this._productType, this._explanation,
+      this._publisher, this._location);
 
   Product.fromMap(Map<String, dynamic> parsedMap, {this.reference})
-      : _name = parsedMap['name'],
+      : _id = parsedMap['id'],
+        _name = parsedMap['name'],
         _productType = parsedMap['productType'],
         _explanation = parsedMap['explanation'],
-        _publisher = parsedMap['publisher'];
+        _publisher = parsedMap['publisher'],
+        _location = parsedMap['location'];
 
   Map<String, dynamic> toMap() {
     return {
+      'id': _id,
       'name': _name,
       'productType': _productType,
       'explanation': _explanation,
       'publisher': _publisher,
+      'location': _location,
     };
   }
+
   Product.fromSnapshot(DocumentSnapshot snapshot)
       : this.fromMap(snapshot.data(), reference: snapshot.reference);
+
+  String get id => _id;
 
   String get publisher => _publisher;
 
   String get explanation => _explanation;
 
-  ProductType get productType => _productType;
+  String get productType => _productType;
 
   String get name => _name;
+
+  @override
+  String toString() {
+    return 'Product{_id: $_id, _name: $_name, _productType: $_productType, _explanation: $_explanation, _publisher: $_publisher, _location: $_location}';
+  }
 }
--- lib/services/FirestoreService.dart
+++ lib/services/FirestoreService.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:f4rtech_gdgsivas_hackathon/app/enums.dart';
+import 'package:f4rtech_gdgsivas_hackathon/models/product.dart';
 import 'package:f4rtech_gdgsivas_hackathon/models/sharer_user.dart';
 import 'package:f4rtech_gdgsivas_hackathon/models/volunteer_user.dart';
 import 'package:f4rtech_gdgsivas_hackathon/services/FirestoreBase.dart';
@@ -7,20 +8,21 @@ import 'package:f4rtech_gdgsivas_hackathon/models/user.dart' as usr;
 
 class FirestoreService implements FirestoreBase {
   @override
-  Future<dynamic> readUser(String uid) async{
+  Future<dynamic> readUser(String uid) async {
     try {
-      var result = await FirebaseFirestore.instance.collection('users').doc(uid).get();
-      if(result != null){
-        if('VOLUNTEER' == result.data()['userType']){
+      var result =
+          await FirebaseFirestore.instance.collection('users').doc(uid).get();
+      if (result != null) {
+        if ('VOLUNTEER' == result.data()['userType']) {
           VolunteerUser user = VolunteerUser.fromSnapshot(result);
           return user;
-        }else if ('SHARER' == result.data()['userType']){
+        } else if ('SHARER' == result.data()['userType']) {
           SharerUser user = SharerUser.fromSnapshot(result);
           return user;
-        }else {
+        } else {
           return null;
         }
-      }else {
+      } else {
         return null;
       }
     } catch (e) {
@@ -31,16 +33,98 @@ class FirestoreService implements FirestoreBase {
 
   @override
   Future<bool> saveUser(dynamic user) async {
-    try{
-      var result = await FirebaseFirestore.instance.collection('users').doc(user.uid).get();
-      if(result != null){
+    try {
+      var result = await FirebaseFirestore.instance
+          .collection('users')
+          .doc(user.uid)
+          .get();
+      if (result != null) {
         await result.reference.set(user.toMap());
         return true;
-      }else {
+      } else {
         return false;
       }
-    }catch (e) {
+    } catch (e) {
       print('saveUser Error: $e');
+      return e;
+    }
+  }
+
+  @override
+  Future<Product> readProduct(String id) async {
+    try {
+      var result =
+          await FirebaseFirestore.instance.collection('products').doc(id).get();
+      if (result != null) {
+        Product product = Product.fromSnapshot(result);
+        return product;
+      } else {
+        return null;
+      }
+    } catch (e) {
+      print('FirestoreService-readProduct Error: $e');
+      return e;
+    }
+  }
+
+  @override
+  Future<bool> saveProduct(Product product) async {
+    try {
+      var result = await FirebaseFirestore.instance
+          .collection('products')
+          .doc(product.id)
+          .get();
+      if (result != null) {
+        await result.reference.set(product.toMap());
+        return true;
+      } else {
+        return false;
+      }
+    } catch (e) {
+      print('FirestoreService-saveProduct Error: $e');
+      return false;
+    }
+  }
+
+  @override
+  Future<List<Product>> readFilteredProducts(GeoPoint location) async {
+    try {
+      var result = await FirebaseFirestore.instance
+          .collection('products')
+          .where('location', isEqualTo: location)
+          .get();
+      if (result != null) {
+        List<Product> products = [];
+        result.docs.forEach((element) {
+          products.add(Product.fromSnapshot(element));
+        });
+        return products;
+      } else {
+        return null;
+      }
+    } catch (e) {
+      print('FirestoreService-readProduct Error: $e');
+      return e;
+    }
+  }
+
+  @override
+  Future<List<Product>> readAllProducts() async {
+    try {
+      var result = await FirebaseFirestore.instance
+          .collection('products')
+          .get();
+      if (result != null) {
+        List<Product> products = [];
+        result.docs.forEach((element) {
+          products.add(Product.fromSnapshot(element));
+        });
+        return products;
+      } else {
+        return null;
+      }
+    } catch (e) {
+      print('FirestoreService-readAllProducts Error: $e');
       return e;
     }
   }
